2acf0bc0638009505dfb6b2557109ba9f9012e4b
Yash Velagapudi <yashv6655@gmail.com>
2021-04-05 22:20:58 -0700

    Setup auth0 on front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,43 @@
 import "./App.css";
 import Login from "./components/Login";
-import { Auth0Provider } from "@auth0/auth0-react";
+import Logout from "./components/Logout";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import axios from "axios";
+import { useUserContext } from "./context/user_context";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <Auth0Provider
-      domain={process.env.REACT_APP_DOMAIN}
-      clientId={process.env.REACT_APP_CLIENT_ID}
-      redirectUri={window.location.origin}
-    >
-      <div className="App">
-        <Login />
+  const { user, isAuthenticated, isLoading } = useAuth0();
+  const [id, setId] = useState("");
+
+  const sendUserId = async () => {
+    const { sub } = user;
+    await axios
+      .post("http://localhost:4000/user", { sub })
+      .catch((err) => console.log(err));
+  };
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
+  if (isAuthenticated) {
+    sendUserId();
+    return (
+      <div>
+        <img src={user.picture} alt={user.name} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+        <Logout />
       </div>
-    </Auth0Provider>
+    );
+  }
+
+  return (
+    <div>
+      <Login />
+    </div>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { UserProvider } from "./context/user_context";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain={process.env.REACT_APP_DOMAIN}
+      clientId={process.env.REACT_APP_CLIENT_ID}
+      redirectUri={window.location.origin}
+      cacheLocation="localstorage"
+    >
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </Auth0Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
